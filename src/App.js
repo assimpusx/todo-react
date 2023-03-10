a6b4216bcd3e08df8ee725a41d242aaf2f6a3551
Pavel <assimpusx@yandex.ru>
2023-03-10 22:28:17 +0700

    add list component in main App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/header/Header";
 import Buttons from "./components/buttons/Buttons";
+import TodoItems from "./components/todoItems/TodoItems";
 
 function App() {
   return (
