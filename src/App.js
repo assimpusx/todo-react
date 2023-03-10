c15c35888348a3142a88d2e7c26881f253b473e0
Pavel <assimpusx@yandex.ru>
2023-03-10 22:54:31 +0700

    add object

--- src/App.js
+++ src/App.js
@@ -3,6 +3,24 @@ import Buttons from "./components/buttons/Buttons";
 import TodoItems from "./components/todoItems/TodoItems";
 
 function App() {
+  const tasks = [
+    {
+      taskName: 'Task #1',
+      taskDescription: 'Do Task #1',
+      taskStatus: 1
+    },
+    {
+      taskName: 'Task #2',
+      taskDescription: 'Do Task #2',
+      taskStatus: 0
+    },
+    {
+      taskName: 'Task #3',
+      taskDescription: 'Do Task #3',
+      taskStatus: 1
+    }
+  ]
+
   return (
     <div className="main">
       <Header></Header>
